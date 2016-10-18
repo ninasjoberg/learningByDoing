dffd16c1951d5cb41b267ed3db13a45dfbb31b1a
Nina Sjöberg <nina.sjoberg@gmail.com>
2016-10-18 11:39:27 +0200

    Det första

---
+++ ninasFirst.js
@@ -0,0 +1,10 @@
+//Här är början på ett nytt projekt. 
+//Vad det ska handla om vet jag ännu inte riktigt.. 
+//Hoppas någon annan kan komma på nått kul. 
+
+var cool = "Monkey";
+var number = 22;
+
+if(cool == "Monkey" && numer ==22){
+	console.log("Hey " + cool + " " + number);
+}
